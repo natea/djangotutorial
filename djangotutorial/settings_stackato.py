1a318ced033ca7d9725302c6702bcb20eb21bd7b
Nate Aune <natea@jazkarta.com>
2012-09-07 18:37:20 -0400

    modernize for newer versions of django

---
+++ djangotutorial/settings_stackato.py
@@ -0,0 +1,10 @@
+import os
+import dj_database_url
+
+from .settings import *
+
+DATABASES = {
+    "default": dj_database_url.config(env="DATABASE_URL"),
+}
+
+MEDIA_ROOT = os.environ['STACKATO_FILESYSTEM']
